821aa65f521e0246d07de957136c6d161e46305a
Akayxn <yujansharma20621129@gmail.com>
2025-07-02 12:57:27 +0545

    added Customer Enitity

---
+++ src/main/java/org/akayxn/entity/Customer.java
@@ -0,0 +1,58 @@
+package org.akayxn.entity;
+
+public class Customer {
+    private String name;
+    private String address;
+    private String email;
+    private String username;
+    private String password;
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
